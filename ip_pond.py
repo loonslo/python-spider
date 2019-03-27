bc09fc50601eda1acd6b10258154340da8aeb87d
slo <chongqing115@126.com>
2019-03-28 00:40:57 +0800

    这些可以用了

--- ip_pond.py
+++ ip_pond.py
@@ -10,10 +10,13 @@ headers = {
 }
 
 # 这是自己的数据库
+# 创建数据库时，使用的字段是ip(varchar)(主键)  port(varchar)  proxy_type(varchar) speed(float)
+
 conn = pymysql.connect(host='127.0.0.1', user='root', passwd='123456', db='jobs', charset='utf8')
 cursor = conn.cursor()
 
 
+# 定义一个随机延时，防止被封ip
 def rand_sleep_time():
     sleep_time = random() * 100
     return time.sleep(sleep_time)
@@ -21,35 +24,37 @@ def rand_sleep_time():
 
 def update_ip_pond():
     # 这个网站目前一共有3637页，这里获取前面的10个页面
-    for i in range(6, 10):
-        # resp = requests.get('https://www.xicidaili.com/nn/%s' % i, headers=headers)
-        # if resp.status_code != 200:
-        #     print('第%s页获取失败' % i)
-        # else:
-        #     print('已获取第%s页内容' % i)
-        # rand_sleep_time()
-        resp = requests.get('https://www.xicidaili.com/nn/6', headers=headers)
-    selector = Selector(text=resp.text)
-    all_items = selector.xpath('//*[@id="ip_list"]//tr')
-    ip_list = []
-    for item in all_items[1:]:
-        # 这里使用xpath从网页提取
-        speed_str = item.xpath('td[7]/div/@title').get()
-        if speed_str:
-            speed = float(speed_str.split('秒')[0])
-        ip = item.xpath('td[2]/text()').get()
-        port = item.xpath('td[3]/text()').get()
-        proxy_type = item.xpath('td[6]/text()').get().lower()
+    for i in range(1, 11):
+        resp = requests.get('https://www.xicidaili.com/nn/%s' % i, headers=headers)
+        if resp.status_code != 200:
+            print('第%s页获取失败' % i)
+        else:
+            print('已获取第%s页内容' % i)
+        # resp = requests.get('https://www.xicidaili.com/nn/7', headers=headers)
+        print(resp)
+        print(resp.text)
+        selector = Selector(text=resp.text)
+        all_items = selector.xpath('//*[@id="ip_list"]//tr')
+        ip_list = []
+        for item in all_items[1:]:
+            # 这里使用xpath从网页提取
+            speed_str = item.xpath('td[7]/div/@title').get()
+            if speed_str:
+                speed = float(speed_str.split('秒')[0])
+            ip = item.xpath('td[2]/text()').get()
+            port = item.xpath('td[3]/text()').get()
+            proxy_type = item.xpath('td[6]/text()').get().lower()
 
-        ip_list.append((ip, port, proxy_type, speed))
+            ip_list.append((ip, port, proxy_type, speed))
 
-        for ip_info in ip_list:
-            # sql的作用为：插入并更新相应的字段
-            cursor.execute(
-                "insert ip_pond(ip,port,proxy_type,speed) values ('{0}','{1}','{2}','{3}') ON DUPLICATE KEY UPDATE ip=VALUES(ip),port=VALUES(port),proxy_type=VALUES(proxy_type),speed=VALUES(speed)"
-                    .format(ip_info[0], ip_info[1], ip_info[2], ip_info[3])
-            )
-        conn.commit()
+            for ip_info in ip_list:
+                # sql的作用为：插入并更新相应的字段
+                cursor.execute(
+                    "insert ip_pond(ip,port,proxy_type,speed) values ('{0}','{1}','{2}','{3}') ON DUPLICATE KEY UPDATE ip=VALUES(ip),port=VALUES(port),proxy_type=VALUES(proxy_type),speed=VALUES(speed)"
+                        .format(ip_info[0], ip_info[1], ip_info[2], ip_info[3])
+                )
+        rand_sleep_time()
+    conn.commit()
 
 
 class GetIp(object):
@@ -136,13 +141,19 @@ class GetIp(object):
 
 
 if __name__ == '__main__':
-    # 运行以下代码之前，需要先运行update_ip_pond()，将数据填入数据库
-    # 创建数据库时，使用的字段是ip(varchar)(主键)  port(varchar)  proxy_type(varchar) speed(float)
-    update_ip_pond()
-    # url = 'https://www.baidu.com'
-    # headers = {
-    #     "User-Agent": "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10"
-    # }
-    # proxies = GetIp().get_proxies()
-    # res = requests.get(url=url, headers=headers, proxies=proxies)
-    # print(res)
+
+    #当取到的ip是https的时候，会有点慢
+    sql = """
+    SELECT * FROM ip_pond
+    """
+    check_table = cursor.execute(sql)
+    if check_table:
+        url = 'https://www.baidu.com'
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10"
+        }
+        proxies = GetIp().get_proxies()
+        res = requests.get(url=url, headers=headers, proxies=proxies)
+    else:
+        update_ip_pond()
+
